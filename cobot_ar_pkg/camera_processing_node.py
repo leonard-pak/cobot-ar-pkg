2195edc7c65281321fc6d2a6730c750f94cc2c43
Leonard Pak <leopak2000@gmail.com>
2023-04-24 11:18:56 +0300

    update processing node

--- cobot_ar_pkg/camera_processing_node.py
+++ cobot_ar_pkg/camera_processing_node.py
@@ -1,51 +1,87 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, Image
 import numpy as np
 from cv_bridge import CvBridge
 import cv2
-from cobot_ar_pkg.detectors import BlobDetector
+from cobot_ar_pkg.detectors import BlobDetectorV2, MatchDetector
+from cobot_ar_pkg.utils import NoDetectionException
 
 
 class CameraProcessing(Node):
     def __init__(self):
         super().__init__('camera_processing')
         self.declare_parameters('', [
-            ('camera_sub_topic',),
-            ('info_pub_topic',),
+            ('mobile_frame_topic',),
+            ('fixed_frame_topic',),
+            ('info_frame_topic',),
             ('rps',)
         ])
-        self.subscription = self.create_subscription(
+        self.subscriberMobileFrame = self.create_subscription(
             CompressedImage,
-            self.get_parameter('camera_sub_topic')
+            self.get_parameter('mobile_frame_topic')
                 .get_parameter_value()
                 .string_value,
-            self.ListenerCallback,
+            self.MobileFrameCallback,
             10
         )
-        self.publisher = self.create_publisher(
+        self.subscriberFixedFrame = self.create_subscription(
+            Image,
+            self.get_parameter('fixed_frame_topic')
+                .get_parameter_value()
+                .string_value,
+            self.FixedFrameCallback,
+            10
+        )
+        self.publisherInfoImage = self.create_publisher(
             CompressedImage,
-            self.get_parameter('info_pub_topic')
+            self.get_parameter('info_frame_topic')
                 .get_parameter_value()
                 .string_value,
             10
         )
         self.bridge = CvBridge()
-        self.infoImage = np.zeros((640, 360, 4), dtype=np.uint8)
+        # Кадр с подвижной камеры на очках
+        self.frameMobile = np.zeros((640, 360, 3), dtype=np.uint8)
+        # Кадр с фиксорованной камеры в помещении
+        self.frameFixed = np.zeros((640, 360, 3), dtype=np.uint8)
         self.timer = self.create_timer(
             1 / self.get_parameter('rps').get_parameter_value().integer_value,
             self.TimerCallback
         )
-        self.detector = BlobDetector()
+        self.featureDetector = BlobDetectorV2()
+        self.matchDetector = MatchDetector()
+
+    def __publishDetection(self):
+        imageWithDetection = self.featureDetector.Detect(self.frameMobile)
+        msg = self.bridge.cv2_to_compressed_imgmsg(imageWithDetection, 'png')
+        self.publisherInfoImage.publish(msg)
+        cv2.imshow('output', imageWithDetection)
 
     def TimerCallback(self):
-        msg = self.bridge.cv2_to_compressed_imgmsg(self.infoImage, 'png')
-        self.publisher.publish(msg)
+        # self.__publishDetection()
+        try:
+            imageAnnotated, rectPoints = self.matchDetector.Detect(
+                # self.frameMobile, self.frameFixed
+                self.frameFixed, self.frameMobile
+            )
+        except NoDetectionException:
+            self.get_logger().warning("No annotated image")
+            return
+        cv2.imshow('orb', imageAnnotated)
+        cv2.waitKey(1)
 
-    def ListenerCallback(self, msg):
-        cvImage = cv2.rotate(
-            self.bridge.compressed_imgmsg_to_cv2(msg), cv2.ROTATE_180)
-        self.infoImage = self.detector.Detect(cvImage)
+    def MobileFrameCallback(self, msg):
+        self.frameMobile = self.bridge.compressed_imgmsg_to_cv2(msg)
+        # self.frameMobile = cv2.rotate(
+        #     self.bridge.compressed_imgmsg_to_cv2(msg),
+        #     cv2.ROTATE_180
+        # )
+        cv2.imshow('mobile_camera', self.frameMobile)
+
+    def FixedFrameCallback(self, msg):
+        self.frameFixed = self.bridge.imgmsg_to_cv2(msg)
+        cv2.imshow('fixed_camera', self.frameFixed)
 
 
 def main(args=None):
